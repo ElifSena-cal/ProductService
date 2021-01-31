b72d888001aef13a009f0afbdaf06fd44111d12a
ElifSena-cal <66389332+ElifSena-cal@users.noreply.github.com>
2021-01-31 23:28:11 +0300

    NorthwindBusiness

---
+++ NorthwindBusiness/Abstract/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using NorthwindEntites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindBusiness.Abstract
+{
+   public interface IProductService
+    {
+        List<Product> GetAll();
+        List<Product> GetProductsByCategory(int categoryId);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
---
+++ NorthwindBusiness/Concrete/ProductManager.cs
@@ -0,0 +1,60 @@
+﻿using NorthwindBusiness.Abstract;
+using NorthwindBusiness.Utilities;
+using NorthwindBusiness.ValidationRules.FluentValidation;
+using NorthwindDataAccess.Abstract;
+using NorthwindDataAccess.Concrete;
+using NorthwindEntites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindBusiness.Concrete
+{
+   public class ProductManager:IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            ValidationTool.Validate(new ProductValidator(), product);
+
+           _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            try
+            {
+                _productDal.Delete(product);
+            }
+            catch (DbUpdateException )
+            {
+
+                throw new Exception("Silme Gerçekleşmedi");
+            }
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetProductsByCategory(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+    }
+
